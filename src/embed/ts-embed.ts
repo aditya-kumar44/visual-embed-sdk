078f94edb0d6f46a3ac8b34910b666481c920e24
adityakumar44 <aditya.kumar1@thoughtspot.com>
2021-09-16 20:26:01 +0530

    fix: lint error

--- src/embed/ts-embed.ts
+++ src/embed/ts-embed.ts
@@ -45,7 +45,7 @@ const V1EventMap = {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface LayoutConfig { }
+export interface LayoutConfig {}
 
 /**
  * Embedded iFrame configuration
@@ -309,8 +309,9 @@ export class TsEmbed {
         const queryStringFrag = queryString ? `&${queryString}` : '';
         const primaryNavParam = `&primaryNavHidden=${!showPrimaryNavbar}`;
         const disableProfileAndHelpParam = `&profileAndHelpInNavBarHidden=${disableProfileAndHelp}`;
-        let queryParams = `?embedApp=true${isAppEmbed ? primaryNavParam : ''}${isAppEmbed ? disableProfileAndHelpParam : ''
-            }${queryStringFrag}`;
+        let queryParams = `?embedApp=true${isAppEmbed ? primaryNavParam : ''}${
+            isAppEmbed ? disableProfileAndHelpParam : ''
+        }${queryStringFrag}`;
         if (this.shouldEncodeUrlQueryParams) {
             queryParams = `?base64UrlEncodedFlags=${getEncodedQueryParamsString(
                 queryParams.substr(1),
@@ -527,8 +528,9 @@ export class TsEmbed {
         if (iframeSrc) {
             const embedPath = '#/embed';
             const currentPath = iframeSrc.includes(embedPath) ? embedPath : '#';
-            this.iFrame.src = `${iframeSrc.split(currentPath)[0]
-                }${currentPath}/${path.replace(/^\/?#?\//, '')}`;
+            this.iFrame.src = `${
+                iframeSrc.split(currentPath)[0]
+            }${currentPath}/${path.replace(/^\/?#?\//, '')}`;
         } else {
             console.log('Please call render before invoking this method');
         }
